747cd0d4b6d45caf9e9f162df9767f98af79e233
Fabian Cardenas <fcardenasaldana@gmail.com>
2023-08-06 15:37:46 -0600

    Repositorio Inicial para un proyecto de React Ts y Eslint con Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/function-component-definition': 0,
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+  console.log('hola');
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
